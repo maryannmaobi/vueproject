387fcc1bf99d92fe7cdc954a1c67b1658a061b3e
maryannmaobi <53537249+maryannmaobi@users.noreply.github.com>
2020-10-19 15:50:31 -0700

    Update loginuser.php

--- food_app/src/assets/food_php/loginuser.php
+++ food_app/src/assets/food_php/loginuser.php
@@ -7,17 +7,25 @@ $username =  $_POST['username'];
 $password =  $_POST['password'];
 
 
-    $sql = "SELECT * from users where username='$username' and password='$password'";
-	$query = $mysqli->query($sql);
-	if($query->num_rows>0){
-		$row=$query->fetch_array();
-        $_SESSION['user_id']=$row['user_id'];
-        // echo $_SESSION['user_id'];
-		$out['message'] = "Login Successful";
-	}
-	else{
-		$out['error'] = true;
-		$out['message'] = "Login Failed. User not Found";
+	$sql = "SELECT * from users where username='$username' and password='$password'";
+	$check = mysqli_query($mysqli, $sql);
+	$row = mysqli_fetch_all($check, MYSQLI_ASSOC);
+
+	foreach ($row as $value) {
+        $_SESSION['user_id'] = $value['user_id'];
+        
     }
+if(isset($_SESSION['user_id'])){
+    $res ='{"success": true}';
+    // echo $_SESSION['user_id'];
+}else{
+    $res ='{"success": false}';
+}
+if($row ){
+    $res = array('success' => true);
+}else{
+    $res = array('success' => false);
+}
+echo json_encode($res);
 
 ?>
