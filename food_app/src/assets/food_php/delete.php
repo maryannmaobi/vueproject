5bd8e45959446fc7645b2564dfd4972042433af8
maryannmaobi <53537249+maryannmaobi@users.noreply.github.com>
2020-10-23 09:42:36 -0700

    Update delete.php

--- food_app/src/assets/food_php/delete.php
+++ food_app/src/assets/food_php/delete.php
@@ -2,7 +2,7 @@
 require 'database.php';
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-$id = $_POST['food_id'];
+$id = $_GET['id'];
 $query = "DELETE FROM added_to_cart WHERE food_id = $id";
 $del_food = mysqli_query($mysqli,$query);
  echo json_encode($del_food);
